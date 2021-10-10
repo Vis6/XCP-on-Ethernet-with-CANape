fad1ea6fd7ad20e3eaa8488ef81b099db72ab5f7
Ximu Zhang <ximuzh@gmail.com>
2021-10-10 15:45:02 -0400

    project update

--- XCPonEthernet/XCPonEthernet.sdk/xcp_freertos/src/xcp_freertos_main.c
+++ XCPonEthernet/XCPonEthernet.sdk/xcp_freertos/src/xcp_freertos_main.c
@@ -101,79 +101,33 @@
 /* Xilinx includes. */
 #include "xil_printf.h"
 #include "xparameters.h"
+#include "xgpiops.h"
 
-#define TIMER_ID	1
-#define DELAY_10_SECONDS	10000UL
-#define DELAY_1_SECOND		1000UL
-#define TIMER_CHECK_THRESHOLD	9
-/*-----------------------------------------------------------*/
 
-/* The Tx and Rx tasks as described at the top of this file. */
-static void prvTxTask( void *pvParameters );
-static void prvRxTask( void *pvParameters );
-static void vTimerCallback( TimerHandle_t pxTimer );
-/*-----------------------------------------------------------*/
+/*
+ * PS GPIO
+ */
+XGpioPs_Config *PS_GPIO_Config;
+XGpioPs PS_Gpio;
+int led_status = 0;
+const int OUTPUT_PORT = 7;
 
-/* The queue used by the Tx and Rx tasks, as described at the top of this
-file. */
-static TaskHandle_t xTxTask;
-static TaskHandle_t xRxTask;
-static QueueHandle_t xQueue = NULL;
-static TimerHandle_t xTimer = NULL;
-char HWstring[15] = "Hello World";
-long RxtaskCntr = 0;
 
+u32 XCP_100us_Counter; // XCP
+
+/*
+ * External functions.
+ */
+extern void mainPeriodicTasks();
+extern void xcpEthInit();
+
+/*
+ * Main function.
+ */
 int main( void )
 {
-	const TickType_t x10seconds = pdMS_TO_TICKS( DELAY_10_SECONDS );
-
-	xil_printf( "Hello from Freertos example main\r\n" );
-
-	/* Create the two tasks.  The Tx task is given a lower priority than the
-	Rx task, so the Rx task will leave the Blocked state and pre-empt the Tx
-	task as soon as the Tx task places an item in the queue. */
-	xTaskCreate( 	prvTxTask, 					/* The function that implements the task. */
-					( const char * ) "Tx", 		/* Text name for the task, provided to assist debugging only. */
-					configMINIMAL_STACK_SIZE, 	/* The stack allocated to the task. */
-					NULL, 						/* The task parameter is not used, so set to NULL. */
-					tskIDLE_PRIORITY,			/* The task runs at the idle priority. */
-					&xTxTask );
-
-	xTaskCreate( prvRxTask,
-				 ( const char * ) "GB",
-				 configMINIMAL_STACK_SIZE,
-				 NULL,
-				 tskIDLE_PRIORITY + 1,
-				 &xRxTask );
-
-	/* Create the queue used by the tasks.  The Rx task has a higher priority
-	than the Tx task, so will preempt the Tx task and remove values from the
-	queue as soon as the Tx task writes to the queue - therefore the queue can
-	never have more than one item in it. */
-	xQueue = xQueueCreate( 	1,						/* There is only one space in the queue. */
-							sizeof( HWstring ) );	/* Each space in the queue is large enough to hold a uint32_t. */
-
-	/* Check the queue was created. */
-	configASSERT( xQueue );
-
-	/* Create a timer with a timer expiry of 10 seconds. The timer would expire
-	 after 10 seconds and the timer call back would get called. In the timer call back
-	 checks are done to ensure that the tasks have been running properly till then.
-	 The tasks are deleted in the timer call back and a message is printed to convey that
-	 the example has run successfully.
-	 The timer expiry is set to 10 seconds and the timer set to not auto reload. */
-	xTimer = xTimerCreate( (const char *) "Timer",
-							x10seconds,
-							pdFALSE,
-							(void *) TIMER_ID,
-							vTimerCallback);
-	/* Check the timer was created. */
-	configASSERT( xTimer );
-
-	/* start the timer with a block time of 0 ticks. This means as soon
-	   as the schedule starts the timer will start running and will expire after
-	   10 seconds */
-	xTimerStart( xTimer, 0 );
+	xcpEthInit(); // initialize tcp/udp and xcp
+	mainPeriodicTasks(); // create periodic tasks
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -186,67 +140,23 @@ int main( void )
 	for( ;; );
 }
 
-
-/*-----------------------------------------------------------*/
-static void prvTxTask( void *pvParameters )
+/*
+ * Other functions definition.
+ */
+void psLEDInit()
 {
-const TickType_t x1second = pdMS_TO_TICKS( DELAY_1_SECOND );
+	// Gpio Init
+	int Status;
 
-	for( ;; )
-	{
-		/* Delay for 1 second. */
-		vTaskDelay( x1second );
+	// gpio configuration
+	PS_GPIO_Config = XGpioPs_LookupConfig(XPAR_PS7_GPIO_0_DEVICE_ID);
+	Status = XGpioPs_CfgInitialize(&PS_Gpio, PS_GPIO_Config, PS_GPIO_Config->BaseAddr);
+	if(Status != XST_SUCCESS)
+		xil_printf("PS MIO GPIO Initialize failed!\n");
+	else
+		xil_printf("PS MIO GPIO Initialize succeed!\n");
 
-		/* Send the next value on the queue.  The queue should always be
-		empty at this point so a block time of 0 is used. */
-		xQueueSend( xQueue,			/* The queue being written to. */
-					HWstring, /* The address of the data being sent. */
-					0UL );			/* The block time. */
-	}
+	XGpioPs_SetDirectionPin(&PS_Gpio, OUTPUT_PORT, 1);
+	XGpioPs_SetOutputEnablePin(&PS_Gpio, OUTPUT_PORT, 1);
+	XGpioPs_WritePin(&PS_Gpio, OUTPUT_PORT, led_status);
 }
-
-/*-----------------------------------------------------------*/
-static void prvRxTask( void *pvParameters )
-{
-char Recdstring[15] = "";
-
-	for( ;; )
-	{
-		/* Block to wait for data arriving on the queue. */
-		xQueueReceive( 	xQueue,				/* The queue being read. */
-						Recdstring,	/* Data is read into this address. */
-						portMAX_DELAY );	/* Wait without a timeout for data. */
-
-		/* Print the received data. */
-		xil_printf( "Rx task received string from Tx task: %s\r\n", Recdstring );
-		RxtaskCntr++;
-	}
-}
-
-/*-----------------------------------------------------------*/
-static void vTimerCallback( TimerHandle_t pxTimer )
-{
-	long lTimerId;
-	configASSERT( pxTimer );
-
-	lTimerId = ( long ) pvTimerGetTimerID( pxTimer );
-
-	if (lTimerId != TIMER_ID) {
-		xil_printf("FreeRTOS Hello World Example FAILED");
-	}
-
-	/* If the RxtaskCntr is updated every time the Rx task is called. The
-	 Rx task is called every time the Tx task sends a message. The Tx task
-	 sends a message every 1 second.
-	 The timer expires after 10 seconds. We expect the RxtaskCntr to at least
-	 have a value of 9 (TIMER_CHECK_THRESHOLD) when the timer expires. */
-	if (RxtaskCntr >= TIMER_CHECK_THRESHOLD) {
-		xil_printf("FreeRTOS Hello World Example PASSED");
-	} else {
-		xil_printf("FreeRTOS Hello World Example FAILED");
-	}
-
-	vTaskDelete( xRxTask );
-	vTaskDelete( xTxTask );
-}
-
